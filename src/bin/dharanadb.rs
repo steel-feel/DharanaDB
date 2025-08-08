60d0c0a6f982eff7c2b06dd39cbbf7678809a4d3
himank jain <himankjain@yahoo.in>
2025-08-09 04:08:25 +0530

    basic cli

---
+++ src/bin/dharanadb.rs
@@ -0,0 +1,38 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Set the key for the value
+    Set { key: String, value: String },
+    /// Get the value
+    Get { key : String },
+    /// Remove the entry for the key
+    Remove {key : String},
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Set { key, value } => {
+            println!("Setting the {} for {} . . . .", value, key);
+        },
+        Commands::Get { key } => {
+              println!("Fetching value for {key}");
+        },
+        Commands::Remove { key } => {
+            println!("Removing key : {key}");
+        }
+
+    }
+}
